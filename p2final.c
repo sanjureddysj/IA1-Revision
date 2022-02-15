ff548f76e1f1f37a991968cfc05a01db8399014d
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2022-02-15 04:02:47 +0000

    p2final Executed

---
+++ p2final.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int input()
+{
+  int a;
+  printf("Enter the Three Numbers\n");
+  scanf("%d",&a);
+  return a;
+}
+
+int compare(int a,int b, int c)
+{
+  int large;
+  if((a>b)&&(a>c))
+  {
+    large=a;
+  }
+
+   else if((b>a)&&(b>c))
+  {
+    large=b;
+  }
+
+  else
+   {
+     large=c;
+   }
+  return large;
+}
+
+void output(int a,int b,int c,int large)
+{
+  printf("The Greatest Number is %d", large);
+}
+
+int main()
+{
+  int a,b,c,large;
+  a=input();
+  b=input();
+  c=input();
+  large=compare(a,b,c);
+  output(a,b,c,large);
+  return 0;
+}
